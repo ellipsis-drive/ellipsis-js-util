c0f16d337b363e01cc79d8f84eb3ad92d9d296f4
Jelle Keulemans <jelle.keulemans@gmail.com>
2022-02-01 22:09:04 +0100

    add url encoding

--- src/lib/RasterLayerUtil.js
+++ src/lib/RasterLayerUtil.js
@@ -3,7 +3,7 @@ import EllipsisApi from "./EllipsisApi";
 const getSlippyMapUrl = (options = {}) => {
 
     if (options.visualization) {
-        let url = `${EllipsisApi.getApiUrl()}/settings/mapLayers/preview/${options.blockId}/${options.captureId}/${options.visualization.method}/{z}/{x}/{y}?parameters=${JSON.stringify(options.visualization.parameters)}`;
+        let url = `${EllipsisApi.getApiUrl()}/settings/mapLayers/preview/${options.blockId}/${options.captureId}/${encodeURIComponent(options.visualization.method)}/{z}/{x}/{y}?parameters=${encodeURIComponent(JSON.stringify(options.visualization.parameters))}`;
         if (options.token) url += '&token=' + options.token;
         return url;
     }
@@ -13,6 +13,6 @@ const getSlippyMapUrl = (options = {}) => {
     return url;
 }
 
-const getLayerId = (options = {}) => `${options.blockId}_${options.captureId}_${options.visualizationId}_${options.visualization ? JSON.stringify(options.visualization) : 'novis'}`;
+const getLayerId = (options = {}) => `${options.blockId}_${options.captureId}_${options.visualizationId}_${options.visualization ? encodeURIComponent(JSON.stringify(options.visualization)) : 'novis'}`;
 
 export { getSlippyMapUrl, getLayerId };
